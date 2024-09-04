80c793f44b26b71d2b37a8f81f9554607e944bed
Edgardo Rodriguez <edgardorodriguezdev@gmail.com>
2024-09-03 21:33:58 -0600

    cambiando la baseURL de la api en produccion

--- frontend/conference/lib/api-service.ts
+++ frontend/conference/lib/api-service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:5110/api",
+  baseURL: "https://conferencesapi.somee.com/api",
   headers: {
     "Content-Type": "application/json",
   },
